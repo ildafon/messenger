da5f2021da3faa764456ff3f9e5aaa457a14c79b
ildafon <ildafon@yandex.ru>
2018-10-04 12:01:06 +0300

    user actions

--- src/app/actions/users.actions.ts
+++ src/app/actions/users.actions.ts
@@ -4,9 +4,28 @@ import { User } from './../models/user.model';
 
 export enum UserActionTypes {
   LOAD = '[Users] Load',
-  LOAD_SUCCESS = '[Users] Load Success',
-  LOAD_ERROR = '[Users] LOAD Error',
+  LOAD_COMPLETE = '[Users] Load Complete',
   SELECT = '[Users] Select One'
 }
 
+export class LoadAction implements Action {
+  readonly type = UserActionTypes.LOAD;
+}
+
+export class LoadCompleteAction implements Action {
+  readonly type = UserActionTypes.LOAD_COMPLETE;
+
+  constructor(public payload: User[]) {}
+}
+
+export class SelectAction implements Action {
+  readonly type = UserActionTypes.SELECT;
+
+  constructor(public payload: string) {}
+}
+
 export type UserActions =
+  LoadAction
+| LoadCompleteAction
+| SelectAction;
+
